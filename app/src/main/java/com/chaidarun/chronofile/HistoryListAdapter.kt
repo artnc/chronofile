1ef417fd7dacccf5c1bedd64cc70c765ec941c82
Art Chaidarun <artchaidarun@gmail.com>
2017-12-27 21:32:23 -0500

    Increase font size and margin

--- app/src/main/java/com/chaidarun/chronofile/HistoryListAdapter.kt
+++ app/src/main/java/com/chaidarun/chronofile/HistoryListAdapter.kt
@@ -1,14 +1,17 @@
 package com.chaidarun.chronofile
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import org.jetbrains.anko.find
 import java.text.SimpleDateFormat
 import java.util.*
 
 class HistoryListAdapter(private val items: List<Entry>, private val itemClick: (Entry) -> Unit) : RecyclerView.Adapter<HistoryListAdapter.ViewHolder>() {
 
-  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(TextView(parent.context), itemClick, this)
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.history_entry, parent, false), itemClick, this)
 
   override fun onBindViewHolder(holder: ViewHolder, position: Int) {
     holder.bindEntry(items[position])
@@ -16,10 +19,13 @@ class HistoryListAdapter(private val items: List<Entry>, private val itemClick: 
 
   override fun getItemCount() = items.size
 
-  class ViewHolder(private val textView: TextView, private val itemClick: (Entry) -> Unit, private val adapter: HistoryListAdapter) : RecyclerView.ViewHolder(textView) {
+  class ViewHolder(view: View, private val itemClick: (Entry) -> Unit, private val adapter: HistoryListAdapter) : RecyclerView.ViewHolder(view) {
+
+    private val textView = view.find<TextView>(R.id.activity)
+
     fun bindEntry(entry: Entry) {
       with(entry) {
-        val date = SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(Date(startTime * 1000))
+        val date = SimpleDateFormat("MMM dd HH:mm").format(Date(startTime * 1000))
         textView.text = "$date $activity"
         textView.setOnClickListener {
           itemClick(this)
