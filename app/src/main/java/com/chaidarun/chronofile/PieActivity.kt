b21f87d4df17ba0038937e3beafd74babadc16b0
Art Chaidarun <artchaidarun@gmail.com>
2017-12-28 11:50:50 -0500

    Pretty print durations in pie chart

--- app/src/main/java/com/chaidarun/chronofile/PieActivity.kt
+++ app/src/main/java/com/chaidarun/chronofile/PieActivity.kt
@@ -2,10 +2,10 @@ package com.chaidarun.chronofile
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.IValueFormatter
 import com.github.mikephil.charting.utils.ColorTemplate
 import kotlinx.android.synthetic.main.activity_pie.*
 import org.jetbrains.anko.collections.forEachReversedByIndex
@@ -16,10 +16,10 @@ class PieActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_pie)
 
-    pieChart.legend.form = Legend.LegendForm.CIRCLE
-    pieChart.legend.position = Legend.LegendPosition.LEFT_OF_CHART
-    pieChart.setTransparentCircleAlpha(0)
-    pieChart.setDrawEntryLabels(true)
+    chart.description.isEnabled = false
+    chart.legend.isEnabled = false
+    chart.setTransparentCircleAlpha(0)
+    chart.setDrawEntryLabels(true)
     setData()
   }
 
@@ -39,7 +39,28 @@ class PieActivity : AppCompatActivity() {
     val pieDataSet = PieDataSet(pieEntries, "Time")
     pieDataSet.colors = ColorTemplate.VORDIPLOM_COLORS.toList() + ColorTemplate.JOYFUL_COLORS.toList()
     pieDataSet.valueTextSize = 12f
-    pieChart.data = PieData(pieDataSet)
-    pieChart.invalidate()
+    pieDataSet.valueFormatter = IValueFormatter { value, _, _, _ -> formatSeconds(value.toLong()) }
+    chart.data = PieData(pieDataSet)
+    chart.invalidate()
+  }
+
+  /** Pretty-prints time in seconds, e.g. 86461 -> "1d 1m" */
+  private fun formatSeconds(seconds: Long): String {
+    val pieces = mutableListOf<String>()
+    val totalMinutes = seconds / 60
+    val minutes = totalMinutes % 60
+    if (minutes != 0L) {
+      pieces.add(0, "${minutes}m")
+    }
+    val totalHours = totalMinutes / 60
+    val hours = totalHours % 24
+    if (hours != 0L) {
+      pieces.add(0, "${hours}h")
+    }
+    val days = totalHours / 24
+    if (days != 0L) {
+      pieces.add(0, "${days}d")
+    }
+    return pieces.joinToString(" ")
   }
 }
