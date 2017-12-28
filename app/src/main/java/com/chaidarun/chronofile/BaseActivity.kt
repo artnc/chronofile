5c0a8cda113900f124a7430421cc97c410f246b5
Art Chaidarun <artchaidarun@gmail.com>
2017-12-28 13:54:25 -0500

    Keep screen awake

--- app/src/main/java/com/chaidarun/chronofile/BaseActivity.kt
+++ app/src/main/java/com/chaidarun/chronofile/BaseActivity.kt
@@ -1,7 +1,13 @@
 package com.chaidarun.chronofile
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
 
 open class BaseActivity : AppCompatActivity() {
 
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+  }
 }
