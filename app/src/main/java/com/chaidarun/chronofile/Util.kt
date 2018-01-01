362623e0ad1416dbaa6f7655aa250eeacf27bb19
Art Chaidarun <artchaidarun@gmail.com>
2018-01-01 18:31:26 -0500

    Round seconds to nearest minute before formatting

--- app/src/main/java/com/chaidarun/chronofile/Util.kt
+++ app/src/main/java/com/chaidarun/chronofile/Util.kt
@@ -49,8 +49,11 @@ fun epochSeconds() = System.currentTimeMillis() / 1000
 
 /** Pretty-prints time given in seconds, e.g. 86461 -> "1d 1m" */
 fun formatTime(seconds: Long): String {
+  // Rounds to nearest minute
+  val adjustedSeconds = if (seconds % 60 < 30) seconds else seconds + 60
+
   val pieces = mutableListOf<String>()
-  val totalMinutes = seconds / 60
+  val totalMinutes = adjustedSeconds / 60
   val minutes = totalMinutes % 60
   if (minutes != 0L) {
     pieces.add(0, "${minutes}m")
