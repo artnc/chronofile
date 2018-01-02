5f03a4a826229e8d9ca0186701ffbb12b4aa1cfe
Art Chaidarun <artchaidarun@gmail.com>
2018-01-01 23:07:04 -0500

    Tweak date format

--- app/src/main/java/com/chaidarun/chronofile/Constants.kt
+++ app/src/main/java/com/chaidarun/chronofile/Constants.kt
@@ -6,7 +6,7 @@ import java.util.*
 /** Logging tag */
 const val TAG = "Chronofile"
 
-val DATE_FORMAT = SimpleDateFormat("EE, dd MMM yyyy", Locale.getDefault())
+val DATE_FORMAT = SimpleDateFormat("EE, d MMM yyyy", Locale.getDefault())
 val TIME_FORMAT = SimpleDateFormat("H:mm", Locale.getDefault())
 
 /** Number of seconds in a day */
