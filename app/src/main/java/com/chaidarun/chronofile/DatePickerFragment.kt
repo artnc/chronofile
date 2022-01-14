2a68ed8bd1ac24c18ad465c838665b2a2c60691f
Daniele Ricci <daniele@casaricci.it>
2022-01-14 21:10:27 +0100

    Lints

--- app/src/main/java/com/chaidarun/chronofile/DatePickerFragment.kt
+++ app/src/main/java/com/chaidarun/chronofile/DatePickerFragment.kt
@@ -11,17 +11,17 @@ import java.util.GregorianCalendar
 class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
 
   override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-    val timestamp = checkNotNull(arguments).getLong(TIMESTAMP)
+    val timestamp = requireArguments().getLong(TIMESTAMP)
     val c = Calendar.getInstance().apply { timeInMillis = timestamp * 1000 }
     val year = c.get(Calendar.YEAR)
     val month = c.get(Calendar.MONTH)
     val day = c.get(Calendar.DAY_OF_MONTH)
-    return DatePickerDialog(checkNotNull(activity), this, year, month, day)
+    return DatePickerDialog(requireActivity(), this, year, month, day)
   }
 
   override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) {
     val timestamp = GregorianCalendar(year, month, day).time.time / 1000
-    when (checkNotNull(arguments).getString(ENDPOINT)) {
+    when (requireArguments().getString(ENDPOINT)) {
       "end" -> Store.dispatch(Action.SetGraphRangeEnd(timestamp))
       "start" -> Store.dispatch(Action.SetGraphRangeStart(timestamp))
       else -> throw Error("Invalid argument for DatePickerFragment")
